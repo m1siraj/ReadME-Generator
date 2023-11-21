5c4860e9dd73b8d2d64c569f92adce8ccd921479
Shaan <22236218+ShaanCoding@users.noreply.github.com>
2023-11-21 21:59:57 +1100

    Base Project Structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import DefaultLayout from "./layouts/Default";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Home from "./pages/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient();
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools").then((mod) => ({
+    default: mod.ReactQueryDevtools,
+  }))
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      // {
+      //   path: "/about-us",
+      //   element: <AboutUs />,
+      // },
+      // {
+      //   path: "/store",
+      //   children: [
+      //     {
+      //       index: true,
+      //       element: <Store />,
+      //     },
+      //     {
+      //       path: "/store/:id",
+      //       element: <StoreID />,
+      //     },
+      //   ],
+      // },
+      // {
+      //   path: "/profile",
+      //   // loader: profileLoader(queryClient),
+      //   children: [
+      //     {
+      //       index: true,
+      //       element: <Profile />,
+      //     },
+      //     {
+      //       path: "/profile/purchased/:id",
+      //       element: <ProfileID />,
+      //     },
+      //   ],
+      // },
+    ],
+  },
+]);
+
+export default function App() {
+  const [showDevtools, setShowDevtools] = React.useState(false);
+
+  React.useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+      {showDevtools && (
+        <React.Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </React.Suspense>
+      )}
+    </QueryClientProvider>
+  );
 }
-
-export default App
